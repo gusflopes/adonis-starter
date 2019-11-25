7d6b96b79f1c4ad4eb31c66aa8b55cd482afb3eb
gusflopes <gusflopes86@gmail.com>
2019-11-25 03:54:49 -0400

    feat: Project Controller -under_development

--- app/Controllers/Http/ProjectController.js
+++ app/Controllers/Http/ProjectController.js
@@ -4,6 +4,8 @@
 /** @typedef {import('@adonisjs/framework/src/Response')} Response */
 /** @typedef {import('@adonisjs/framework/src/View')} View */
 
+const Project = use('App/Models/Project')
+
 /**
  * Resourceful controller for interacting with projects
  */
@@ -17,19 +19,10 @@ class ProjectController {
    * @param {Response} ctx.response
    * @param {View} ctx.view
    */
-  async index ({ request, response, view }) {
-  }
+  async index ({ request, response }) {
+    const projects = await Project.all()
 
-  /**
-   * Render a form to be used for creating a new project.
-   * GET projects/create
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   * @param {View} ctx.view
-   */
-  async create ({ request, response, view }) {
+    return projects
   }
 
   /**
@@ -40,7 +33,12 @@ class ProjectController {
    * @param {Request} ctx.request
    * @param {Response} ctx.response
    */
-  async store ({ request, response }) {
+  async store ({ request, response, auth }) {
+    const data = request.only(['title', 'description'])
+
+    const project = await Project.create({ ...data, user_id: auth.user.id })
+
+    return project
   }
 
   /**
@@ -53,18 +51,6 @@ class ProjectController {
    * @param {View} ctx.view
    */
   async show ({ params, request, response, view }) {
-  }
-
-  /**
-   * Render a form to update an existing project.
-   * GET projects/:id/edit
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   * @param {View} ctx.view
-   */
-  async edit ({ params, request, response, view }) {
   }
 
   /**
